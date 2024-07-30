fa721d8c251492b7c7b898ae11cff3f1e74bfe3d
Mohd Saqib Ansari <you@example.com>
2024-07-30 17:50:39 +0530

    mergesort

--- Sorting/src/Test.java
+++ Sorting/src/Test.java
@@ -3,10 +3,35 @@ public class Test {
 
         int[] arr = {5, 3, 8, 2, 9, 1};
 
-        int n = arr.length;
-        BubbleSort bs = new BubbleSort();
-        BubbleSort.BS (arr,n);
-        bs.printArray (arr,n);
+        MergeSort ms = new MergeSort ();
+
+        MergeSort.MS ( arr,0,arr.length-1 );
+
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int n = arr.length;
+//        BubbleSort bs = new BubbleSort();
+//        BubbleSort.BS (arr,n);
+//        bs.printArray (arr,n);
 
     }
 }
