3a699234bcb09b9ae2c1369305970d99210d2e1e
Mohd Saqib Ansari <you@example.com>
2024-09-04 23:21:58 +0530

    update

--- LeatCode/src/ContainsDuplicate.java
+++ LeatCode/src/ContainsDuplicate.java
@@ -11,3 +11,22 @@ class Solution {
         return false;
     }
 }
+
+
+//class Test{
+//    public boolean containsDuplicate(int[] nums) {
+//
+//        for(int i=0;i<nums.length-1;i++){
+//
+//            for(int j =i+1; j<nums.length;j++){
+//
+//                if(nums[i] == nums[j]){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//
+//
+//    }
+//}
