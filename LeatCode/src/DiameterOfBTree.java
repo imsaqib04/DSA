f5e9c344df496900a352d1b4943d4781cbf856cd
Mohd Saqib Ansari <you@example.com>
2024-09-05 21:46:20 +0530

    using resursion

---
+++ LeatCode/src/DiameterOfBTree.java
@@ -0,0 +1,37 @@
+/**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+public class DiameterOfBTree {
+     int diameter = 0;
+
+     public int diameterOfBinaryTree(TreeNode root) {
+
+         height ( root );
+         return diameter;     //nodes = edge +1;
+     }
+
+     int height(TreeNode node) {
+
+         if (node == null) {
+             return 0;
+         }
+         int lh = height ( node.left );
+         int rh = height ( node.right );
+         diameter = Math.max ( diameter, lh + rh );
+
+         return Math.max ( lh, rh ) + 1;
+
+     }
+}
