f7cd536994ffa320e6a5edd7e8f8a7089d2cf7f2
Mohd Saqib Ansari <you@example.com>
2024-09-12 23:47:39 +0530

    solved

---
+++ LeatCode/FirstBadVersion.java
@@ -0,0 +1,38 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n){
+
+        int start = 1;
+        int end = n;
+
+        while (start < end){
+            int mid = start + (end-start)/2;
+            if(isBadVersion(mid)){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+/*
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n){
+
+        for (int i = 1; i <= n; i++) {
+            if (isBadVersion(i)) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}*/
