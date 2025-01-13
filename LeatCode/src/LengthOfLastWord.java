a6be0b2abbbf498c41de6888bb8ab2cad463e215
Mohd Saqib Ansari <you@example.com>
2025-01-13 23:59:29 +0530

    uodate

--- LeatCode/src/LengthOfLastWord.java
+++ LeatCode/src/LengthOfLastWord.java
@@ -1,13 +1,39 @@
 public class LengthOfLastWord {
-    public int lengthOfLastWord(String s) {
-        int i = s.length () - 1;
-        while (i >= 0 && s.charAt ( i ) == ' ') {
-            --i;
+
+    int lengthOfLastWord(String s) {
+
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            if (s.charAt(i) != ' ') {
+                count++;
+            }
+            else {
+                // it's a white space instead
+                // Did we already started to count a word ?
+                // Yes so we found the last word
+                if (count > 0)
+                    return count;
+            }
         }
-        int j = i;
-        while (j >= 0 && s.charAt ( j ) != ' ') {
-            --j;
-        }
-        return i - j;
+
+        return count;
     }
+
 }
+
+//public class LengthOfLastWord {
+//    public int lengthOfLastWord(String s) {
+//        int i = s.length () - 1;
+//        while (i >= 0 && s.charAt ( i ) == ' ') {
+//            --i;
+//        }
+//        int j = i;
+//        while (j >= 0 && s.charAt ( j ) != ' ') {
+//            --j;
+//        }
+//        return i - j;
+//    }
+//}
+
