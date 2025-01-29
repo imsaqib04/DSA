d8424ede87fffbd3bd8511042cb47905ffaf609f
Mohd Saqib Ansari <you@example.com>
2025-01-29 23:39:47 +0530

    done

--- LeatCode/src/RansomNote.java
+++ LeatCode/src/RansomNote.java
@@ -2,33 +2,37 @@ import java.util.HashMap;
 class RansomNote {
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<> ();
 
-        for(int i = 0;i<magazine.length();i++){
-            char c = magazine.charAt(i);
-            if(!map.containsKey(c)){
-                map.put(c,1);
-            }
-            else{
-                map.put(c,map.get(c)+1);
+        for (int i = 0; i < magazine.length (); i++) {
+            char c = magazine.charAt ( i );
+            if (!map.containsKey ( c )) {
+                map.put ( c, 1 );
+            } else {
+                map.put ( c, map.get ( c ) + 1 );
             }
         }
 
-        for(int i =0;i<ransomNote.length();i++){
-            char c = ransomNote.charAt(i);
-            if(!map.containsKey(c)){
-                return false;
-            }
-            else{
-                map.put(c,map.get(c)-1);
+        int count = 0;
+        for (int i = 0; i < ransomNote.length (); i++) {
+            char c = ransomNote.charAt ( i );
+            if (map.containsKey ( c ) && map.get ( c ) > 0) {
+                count++;
+                map.put ( c, map.get ( c ) - 1 );
+            } else {
+                break;
             }
         }
-        return true;
+        if (count == ransomNote.length ()) {
+
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         RansomNote r = new RansomNote ();
-        Boolean b = r.canConstruct ( "ab","aab" );
+        Boolean b = r.canConstruct ( "aa","ab" );
         System.out.println (b);
     }
 }
