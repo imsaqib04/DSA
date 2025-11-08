2778ffb46a8b99a6fb857ccb4f3258f96481c994
saqib <saqibansari2011@gmail.com>
2025-11-08 17:28:14 +0530

    subarray sum of k

---
+++ LeatCode/src/SubarraySumEqualsK.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<> ();
+        int count = 0;
+        int sum = 0;
+        map.put ( 0, 1 );
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+
+            if (map.containsKey ( sum - k )) {
+                count = count + map.get ( sum - k );
+            }
+            map.put ( sum, map.getOrDefault ( sum, 0 ) + 1 );
+        }
+
+        return count;
+
+
+        // int n = nums.length;
+        // int count = 0;
+
+        // for(int i = 0;i<n;i++){
+        //     int sum = 0;
+        //     for(int j = i;j<n;j++){
+        //         sum = sum+nums[j];
+
+        //         if(sum == k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+    }
+}
