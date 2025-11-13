5cc8b5ba6110ecdbce67dba5df3e2afe13339469
saqib <saqibansari2011@gmail.com>
2025-11-13 12:35:33 +0530

    find min

---
+++ LeatCode/src/FindMinInRotatedSortedArray.java
@@ -0,0 +1,30 @@
+class FindMinInRotatedSortedArray {
+    public int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        // If array is already sorted
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+
+        // Binary search
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            // If mid element is greater than right element,
+            // min is in the right part
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                // Otherwise min is in the left part (including mid)
+                right = mid;
+            }
+        }
+
+        // left == right → minimum found
+        return nums[left];
+    }
+}
