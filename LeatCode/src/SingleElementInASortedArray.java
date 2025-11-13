425cef0684ec769d759aacface8f9eab79a6adcf
saqib <saqibansari2011@gmail.com>
2025-11-13 13:15:49 +0530

    bs problem

---
+++ LeatCode/src/SingleElementInASortedArray.java
@@ -0,0 +1,26 @@
+class SingleElementInASortedArray {
+    public int singleNonDuplicate(int[] nums) {
+
+        int left = 0;
+        int right= nums.length-1;
+
+        while(left<right){
+
+            int mid = left + (right-left)/2;
+
+            if (mid%2==1){
+                mid--;
+            }
+
+            if(nums[mid]==nums[mid+1]){   // right side mai hai
+                left = mid+2;
+            }
+            else{
+                right = mid;          // left side mai hai
+            }
+
+
+        }
+        return nums[left];
+    }
+}
