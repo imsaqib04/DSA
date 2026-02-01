d14d33dbaec86738d924a80bfd9268fa262ae5e4
saqib <saqibansari2011@gmail.com>
2026-02-01 16:04:49 +0530

    brute force

---
+++ LeetCode/src/MinimumCost.java
@@ -0,0 +1,20 @@
+class MinimumCost {
+    public int minimumCost(int[] nums) {
+
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 1;i<n-1;i++){
+
+            for(int j = i + 1;j<n;j++){
+
+                int cost = nums[0]+nums[i]+nums[j];
+
+                ans = Math.min(cost,ans);
+            }
+
+        }
+
+        return ans;
+    }
+}
